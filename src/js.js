8ed937ee171e461da4087c64a97d9943acc75354
anettehoglund <hoglundanette@gmail.com>
2024-09-16 15:10:33 +0200

    fixed context bugg

--- src/js.js
+++ src/js.js
@@ -15,7 +15,7 @@ let userInstruction = document.querySelector("#userinput");
 let apiKey = "8f100a03dbef7b07767405e3a2t1o8ce";
 let prompt = `Generate a Crossfit WOD using ${userInstruction.value}`;
 let context =
-  "You are a Crossfit training expert and will generate a metcon listed in bullet points. Answer should be short and shown in basic HTML list";
+  "You are a Crossfit training expert and will generate a metcon. Answer should be short and shown in a list using basic html. only include workout";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 console.log(userInstruction.value)
